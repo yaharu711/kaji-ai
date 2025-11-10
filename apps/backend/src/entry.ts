93a707216d3d1a7a2b2b5c0bc08ea7a4a7df4b1b
yaharu711 <yah7yah707@gmail.com>
2025-11-10 13:00:56 +0900

    add edge server

--- apps/backend/src/entry.ts
+++ apps/backend/src/entry.ts
@@ -1,16 +1,5 @@
 import { serve } from "@hono/node-server";
-import { handle } from "hono/vercel";
 import app from "./routing/index.js";
-
-const runtime = "nodejs" as const;
-
-export const config = {
-  runtime,
-};
-
-console.log(`Before Running in ${runtime} runtime`);
-const handler = handle(app);
-console.log(`Running in ${runtime} runtime`);
 
 if (process.env.VERCEL === undefined) {
   const port = Number(process.env.PORT ?? 3000);
@@ -21,4 +10,4 @@ if (process.env.VERCEL === undefined) {
   console.log(`Local server running at http://localhost:${port}`);
 }
 
-export default handler;
+export default app;
