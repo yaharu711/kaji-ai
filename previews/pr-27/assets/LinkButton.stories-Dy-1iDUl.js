3ac4499ca83b93c2316f9e441dbd96236bcc5c88
yaharu711 <yaharu711@users.noreply.github.com>
2025-12-25 08:10:38 +0000

    deploy: 791329b7e13a37090d719badb2786fbc700c83d0

---
+++ previews/pr-27/assets/LinkButton.stories-Dy-1iDUl.js
@@ -0,0 +1,5 @@
+import{j as o}from"./jsx-runtime-u17CrQMm.js";import{L as s,M as t}from"./index-CRIdII0D.js";import"./iframe-BcHzevJv.js";import"./preload-helper-PPVm8Dsz.js";const p={title:"Components/LinkButton",component:s,parameters:{layout:"centered"},decorators:[a=>o.jsx(t,{children:o.jsx(a,{})})],args:{to:"/",children:"リンクボタン"}},r={},e={args:{variant:"secondary"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:"{}",...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: "secondary"
+  }
+}`,...e.parameters?.docs?.source}}};const d=["Primary","Secondary"];export{r as Primary,e as Secondary,d as __namedExportsOrder,p as default};
