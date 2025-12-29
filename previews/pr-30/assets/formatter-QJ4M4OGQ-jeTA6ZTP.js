c591f97fde0002d3d7b125f393d80b57ea80663c
yaharu711 <yaharu711@users.noreply.github.com>
2025-12-29 09:37:18 +0000

    deploy: daffdae112c741713e03dd4374ff0b20cb6371bf

---
+++ previews/pr-30/assets/formatter-QJ4M4OGQ-jeTA6ZTP.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-GHJI37HO-NWG3QH0m.js";import{G as t}from"./iframe-D-V_QVrn.js";import"./preload-helper-PPVm8Dsz.js";import"./react-18-DBKZ00c7.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
