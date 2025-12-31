f569d425239db3fe92da2179985b47f275c55a49
yaharu711 <yaharu711@users.noreply.github.com>
2025-12-31 09:29:14 +0000

    deploy: 208be159803d5454f6b85ba2505b07659ae4658c

---
+++ previews/pr-34/assets/formatter-QJ4M4OGQ-DA3tHwEP.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-GHJI37HO-CECzX7uZ.js";import{G as t}from"./iframe-52Q5jgRC.js";import"./preload-helper-PPVm8Dsz.js";import"./react-18-BJSXo7Dm.js";import"./index-DLW_x0nD.js";import"./index-Z_e9Z2OQ.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),l=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{l as formatter};
---
+++ previews/pr-34/assets/index-Z_e9Z2OQ.js
@@ -0,0 +1,1 @@
+import{g as r}from"./iframe-52Q5jgRC.js";import{r as o}from"./index-DLW_x0nD.js";var t=o();const m=r(t);export{m as R,t as r};
