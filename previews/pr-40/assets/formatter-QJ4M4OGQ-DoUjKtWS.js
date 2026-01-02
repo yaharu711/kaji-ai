e9e64b0ae5fb023704a347508b3aaccdf6506905
yaharu711 <yaharu711@users.noreply.github.com>
2026-01-02 09:44:06 +0000

    deploy: 9bd90a6b061bb1975f298517715f009a3154323a

---
+++ previews/pr-40/assets/formatter-QJ4M4OGQ-DoUjKtWS.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./DocsRenderer-GHJI37HO-jJOLWnfl.js";import{G as t}from"./iframe-BsvuHyQB.js";import"./preload-helper-PPVm8Dsz.js";import"./react-18-CXrlyE7Q.js";import"./index-BVkoTNZK.js";import"./index-RbqPGz0p.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),l=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{l as formatter};
