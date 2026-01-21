772d7330388250cf21a85268ac42434bdaa4c7fb
yaharu711 <yah7yah707@gmail.com>
2026-01-21 17:57:06 +0900

    modify repository

--- apps/backend/src/repositories/choreBeatings.repository.ts
+++ apps/backend/src/repositories/choreBeatings.repository.ts
@@ -6,14 +6,6 @@ export class ChoreBeatingsRepository {
   constructor(private readonly db: Database) {}
 
   async create(beating: NewChoreBeatingModel): Promise<void> {
-    await this.db.insert(schema.choreBeatings).values({
-      groupId: beating.groupId,
-      choreId: beating.choreId,
-      userId: beating.userId,
-      likeCount: beating.likeCount,
-      beatedAt: beating.beatedAt,
-      createdAt: beating.createdAt,
-      updatedAt: beating.updatedAt,
-    });
+    await this.db.insert(schema.choreBeatings).values(beating);
   }
 }
