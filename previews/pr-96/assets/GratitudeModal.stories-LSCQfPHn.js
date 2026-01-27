d694143ef1bbce232e2b5555ae623d093a6b630a
yaharu711 <yaharu711@users.noreply.github.com>
2026-01-27 07:05:32 +0000

    deploy: 2e84375b44429c16bedd234aa0349fd6373089aa

---
+++ previews/pr-96/assets/GratitudeModal.stories-LSCQfPHn.js
@@ -0,0 +1,14 @@
+import{G as r}from"./index-CW3MEMSV.js";import"./jsx-runtime-u17CrQMm.js";import"./iframe-Cb-SkDV4.js";import"./preload-helper-PPVm8Dsz.js";import"./index-D26oWmff.js";import"./index-jLTPr6cb.js";import"./index-BKJSiE1y.js";import"./Combination-yRFr98uK.js";import"./index-DDGXdEE0.js";import"./index-BbHZ9j1j.js";import"./x-CTIx35C6.js";import"./createLucideIcon-JTP2FxnL.js";import"./index-BOz_el01.js";import"./index-BKtDPDeF.js";import"./index-FcjWBIxF.js";import"./index-Cs5FSBpv.js";import"./index-BDo3CKSt.js";import"./chunk-JMJ3UQ3L-bfmPwixf.js";import"./index-BtCai76y.js";import"./index-24HARjMb.js";import"./index-00cSPT6P.js";import"./index-CruuCqdX.js";import"./index-Bnud37o_.js";import"./index-BjCULmg9.js";import"./index-DZNB0tCQ.js";import"./check-1rHQJ3Dm.js";import"./index-3saN6Hob.js";import"./index-DmGvZV_T.js";import"./heart-CtfKVUS-.js";const j={component:r,parameters:{layout:"fullscreen"}},o={args:{open:!0,onOpenChange:()=>{},userName:"佐藤 次郎",choreName:"料理を討伐",choreIconCode:"cooking",onSubmit:async()=>{}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    open: true,
+    onOpenChange: () => {
+      /* noop */
+    },
+    userName: "佐藤 次郎",
+    choreName: "料理を討伐",
+    choreIconCode: "cooking",
+    onSubmit: async () => {
+      /* noop */
+    }
+  }
+}`,...o.parameters?.docs?.source}}};const q=["Default"];export{o as Default,q as __namedExportsOrder,j as default};
